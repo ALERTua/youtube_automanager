50acce4ed47f81bd5680e0942cf45425d3b2413f
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2024-09-22 12:18:43 +0300

    ! deploy script

--- script/deploy_remote.cmd
+++ script/deploy_remote.cmd
@@ -1,6 +1,6 @@
 
 @echo off
-set DOCKER_HOST=tcp://docker:2375
+if not defined DOCKER_HOST set DOCKER_HOST=tcp://docker:2375
 set DOCKER_REMOTE=1
 call %~dp0\deploy.cmd %*
 where nircmd >nul 2>nul && nircmd beep 500 500
